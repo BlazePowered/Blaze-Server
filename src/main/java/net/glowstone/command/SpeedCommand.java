609730f6a8427b3948c521ef1ba8a3942ca4d183
David <h100000@h100000.com>
2015-11-23 17:46:40 -0600

    Fix speed command inversion and set range to 1 to 10

--- src/main/java/net/glowstone/command/SpeedCommand.java
+++ src/main/java/net/glowstone/command/SpeedCommand.java
@@ -8,138 +8,198 @@ import org.bukkit.entity.Player;
 
 public class SpeedCommand extends BukkitCommand {
 
-	public SpeedCommand() {
-		super("gsspeed");
-		this.description = "Sets the speed of a player or yourself from";
-		this.usageMessage = "/speed <player> <your speed>";
-		this.setPermission("glowstone.command.speed");
-	}
+    public SpeedCommand() {
+        super("gsespeed");
+        this.description = "Sets the speed of a player or yourself from";
+        this.usageMessage = "/gsespeed <player> <your speed>";
+        this.setPermission("glowstone.command.speed");
+    }
 
-	@SuppressWarnings("deprecation")
-	@Override
-	public boolean execute(CommandSender sender, String label, String[] args) {
-		if (sender instanceof Player) {
-			Player pl = (Player) sender;
-			if (args.length <= 0) {
-				sender.sendMessage(ChatColor.RED + "Usage: " + usageMessage);
-				return false;
-			} else {
-				if (args.length == 1) {
-					if (pl.isOnGround()) {
-						float playerSpeed = 0.0F;
-						try {
-							playerSpeed = Float.parseFloat(args[0]);
-							if (playerSpeed > 1.0F) {
-								pl.sendMessage(ChatColor.RED
-										+ "Error: argument too high! (only up to 1.0F)");
-								return false;
-							}
-							if (playerSpeed < -1.0F) {
-								pl.sendMessage(ChatColor.RED
-										+ "Error: argument too low! (only up to -1.0F)");
-								return false;
-							}
-							pl.setWalkSpeed(playerSpeed);
-							pl.sendMessage(ChatColor.GREEN
-									+ "Your walkspeed has been modified to "
-									+ playerSpeed);
-						}
+    @SuppressWarnings("deprecation")
+    @Override
+    public boolean execute(CommandSender sender, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player pl = (Player) sender;
+            if (args.length <= 0) {
+                sender.sendMessage(ChatColor.RED + "Usage: " + usageMessage);
+                return false;
+            } else {
+                if (args.length == 1) {
+                    if (pl.isOnGround()) {
+                        int playerSpeed = 0;
+                        try {
+                            playerSpeed = Integer.parseInt(args[0]);
+                            if (playerSpeed > 10) {
+                                pl.sendMessage(ChatColor.RED
+                                        + "Error: argument too high! (only up to 10)");
+                                return false;
+                            }
+                            if (playerSpeed < 1) {
+                                pl.sendMessage(ChatColor.RED
+                                        + "Error: argument too low! (only down to 1)");
+                                return false;
+                            }
+                            switch (playerSpeed) {
+                                case 10:
+                                    pl.setWalkSpeed(0.1F);
+                                    break;
+                                case 9:
+                                    pl.setWalkSpeed(0.12F);
+                                    break;
+                                case 8:
+                                    pl.setWalkSpeed(0.14F);
+                                    break;
+                                case 7:
+                                    pl.setWalkSpeed(0.16F);
+                                    break;
+                                case 6:
+                                    pl.setWalkSpeed(0.18F);
+                                    break;
+                                case 5:
+                                    pl.setWalkSpeed(0.2F);
+                                    break;
+                                case 4:
+                                    pl.setWalkSpeed(0.3F);
+                                    break;
+                                case 3:
+                                    pl.setWalkSpeed(0.4F);
+                                    break;
+                                case 2:
+                                    pl.setWalkSpeed(0.5F);
+                                    break;
+                                case 1:
+                                    pl.setWalkSpeed(0.6F);
+                                    break;
+                            }
+                            pl.sendMessage(ChatColor.GREEN
+                                    + "Your walkspeed has been modified to "
+                                    + playerSpeed);
+                        } catch (NumberFormatException e) {
+                            pl.sendMessage(ChatColor.RED
+                                    + "Error: invalid speed!");
+                        }
 
-						catch (NumberFormatException e) {
-							pl.sendMessage(ChatColor.RED
-									+ "Error: argument cannot get converted to float!");
-						}
+                    }
+                    if (pl.isFlying()) {
+                        int playerFlyingSpeed = 0;
+                        try {
+                            playerFlyingSpeed = Integer.parseInt(args[0]);
+                            if (playerFlyingSpeed > 10) {
+                                pl.sendMessage(ChatColor.RED
+                                        + "Error: speed too high! (only up to 10)");
+                                return false;
+                            }
+                            if (playerFlyingSpeed < 1) {
+                                pl.sendMessage(ChatColor.RED
+                                        + "Error: argument too low! (only down to 1)");
+                                return false;
+                            }
+                            switch (playerFlyingSpeed) {
+                                case 10:
+                                    pl.setFlySpeed(0.5F);
+                                    break;
+                                case 9:
+                                    pl.setFlySpeed(0.45F);
+                                    break;
+                                case 8:
+                                    pl.setFlySpeed(0.4F);
+                                    break;
+                                case 7:
+                                    pl.setFlySpeed(0.35F);
+                                    break;
+                                case 6:
+                                    pl.setFlySpeed(0.25F);
+                                    break;
+                                case 5:
+                                    pl.setFlySpeed(0.1F);
+                                    break;
+                                case 4:
+                                    pl.setFlySpeed(0.08F);
+                                    break;
+                                case 3:
+                                    pl.setFlySpeed(0.06F);
+                                    break;
+                                case 2:
+                                    pl.setFlySpeed(0.04F);
+                                    break;
+                                case 1:
+                                    pl.setFlySpeed(0.02F);
+                                    break;
+                            }
+                            pl.sendMessage(ChatColor.GREEN
+                                    + "Your flying speed is now "
+                                    + playerFlyingSpeed);
 
-					}
-					if (pl.isFlying()) {
-						float playerFlyingSpeed = 0.0F;
-						try {
-							playerFlyingSpeed = Float.parseFloat(args[0]);
-							if (playerFlyingSpeed > 1.0F) {
-								pl.sendMessage(ChatColor.RED
-										+ "Error: argument too high! (only up to 1.0F)");
-								return false;
-							}
-							if (playerFlyingSpeed < -1.0F) {
-								pl.sendMessage(ChatColor.RED
-										+ "Error: argument too low! (only up to -1.0F)");
-								return false;
-							}
-							pl.setFlySpeed(playerFlyingSpeed);
-							pl.sendMessage(ChatColor.GREEN
-									+ "Your flying speed is now "
-									+ playerFlyingSpeed);
+                        } catch (NumberFormatException e) {
+                            pl.sendMessage(ChatColor.RED
+                                    + "Error: invalid speed!");
+                        }
+                    }
+                }
+                if (args.length == 2) {
+                    Player targetpl = Bukkit.getServer().getPlayer(args[0]);
+                    if (targetpl.isOnGround()) {
+                        if (targetpl == null) {
+                            pl.sendMessage(ChatColor.RED + "Player not found!");
+                        } else {
+                            float pspeed = 0.0F;
+                            try {
+                                pspeed = Float.parseFloat(args[1]);
+                                if (pspeed > 1.0F) {
+                                    pl.sendMessage(ChatColor.RED
+                                            + "Error: argument too high! (only up to 1.0F)");
+                                    return false;
+                                }
+                                if (pspeed < -1.0F) {
+                                    pl.sendMessage(ChatColor.RED
+                                            + "Error: argument too less! (only up to -1.0F)");
+                                    return false;
+                                }
+                                targetpl.setWalkSpeed(pspeed);
+                                targetpl.sendMessage(ChatColor.GREEN
+                                        + "Your walkspeed has been modified to "
+                                        + pspeed);
+                                pl.sendMessage(ChatColor.GREEN
+                                        + targetpl.getName()
+                                        + "'s walkspeed has been modified to "
+                                        + pspeed);
+                            } catch (Exception e) {
+                                pl.sendMessage(ChatColor.RED
+                                        + "Error: argument cannot get converted to float!");
+                            }
+                        }
+                    }
+                    if (targetpl.isFlying()) {
+                        float flyingSpeed = 0.0F;
+                        try {
+                            flyingSpeed = Float.parseFloat(args[1]);
+                            if (flyingSpeed > 1.0F) {
+                                pl.sendMessage(ChatColor.RED
+                                        + "Error: argument too high! (only up to 1.0F)");
+                                return false;
+                            }
+                            if (flyingSpeed < -1.0F) {
+                                pl.sendMessage(ChatColor.RED
+                                        + "Error: argument too less! (only up to -1.0F)");
+                                return false;
+                            }
+                            targetpl.setFlySpeed(flyingSpeed);
+                            targetpl.sendMessage(ChatColor.GREEN
+                                    + "Your flying speed has been modified to "
+                                    + flyingSpeed);
+                            pl.sendMessage(ChatColor.GREEN + targetpl.getName()
+                                    + "'s flying speed has been modified to "
+                                    + flyingSpeed);
+                        } catch (Exception e) {
+                            pl.sendMessage(ChatColor.RED
+                                    + "Error: argument cannot get converted to float!");
+                        }
 
-						} catch (NumberFormatException e) {
-							pl.sendMessage(ChatColor.RED
-									+ "Error: argument cannot get converted to float!");
-						}
-					}
-				}
-				if (args.length == 2) {
-					Player targetpl = Bukkit.getServer().getPlayer(args[0]);
-					if (targetpl.isOnGround()) {
-						if (targetpl == null) {
-							pl.sendMessage(ChatColor.RED + "Player not found!");
-						} else {
-							float pspeed = 0.0F;
-							try {
-								pspeed = Float.parseFloat(args[1]);
-								if (pspeed > 1.0F) {
-									pl.sendMessage(ChatColor.RED
-											+ "Error: argument too high! (only up to 1.0F)");
-									return false;
-								}
-								if (pspeed < -1.0F) {
-									pl.sendMessage(ChatColor.RED
-											+ "Error: argument too less! (only up to -1.0F)");
-									return false;
-								}
-								targetpl.setWalkSpeed(pspeed);
-								targetpl.sendMessage(ChatColor.GREEN
-										+ "Your walkspeed has been modified to "
-										+ pspeed);
-								pl.sendMessage(ChatColor.GREEN
-										+ targetpl.getName()
-										+ "'s walkspeed has been modified to "
-										+ pspeed);
-							} catch (Exception e) {
-								pl.sendMessage(ChatColor.RED
-										+ "Error: argument cannot get converted to float!");
-							}
-						}
-					}
-					if (targetpl.isFlying()) {
-						float flyingSpeed = 0.0F;
-						try {
-							flyingSpeed = Float.parseFloat(args[1]);
-							if (flyingSpeed > 1.0F) {
-								pl.sendMessage(ChatColor.RED
-										+ "Error: argument too high! (only up to 1.0F)");
-								return false;
-							}
-							if (flyingSpeed < -1.0F) {
-								pl.sendMessage(ChatColor.RED
-										+ "Error: argument too less! (only up to -1.0F)");
-								return false;
-							}
-							targetpl.setFlySpeed(flyingSpeed);
-							targetpl.sendMessage(ChatColor.GREEN
-									+ "Your flying speed has been modified to "
-									+ flyingSpeed);
-							pl.sendMessage(ChatColor.GREEN + targetpl.getName()
-									+ "'s flying speed has been modified to "
-									+ flyingSpeed);
-						} catch (Exception e) {
-							pl.sendMessage(ChatColor.RED
-									+ "Error: argument cannot get converted to float!");
-						}
+                    }
+                }
+            }
 
-					}
-				}
-			}
-
-		}
-		return false;
-	}
+        }
+        return false;
+    }
 }
