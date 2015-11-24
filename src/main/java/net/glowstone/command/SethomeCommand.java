84c4a0441a54809c431fd28bcb126b104aa829c4
Bryan Barren <bryanbarren@gmail.com>
2015-11-23 21:29:35 -0500

    Very simple set home.
    Just adding this simple set home method so it's filled with something.

--- src/main/java/net/glowstone/command/SethomeCommand.java
+++ src/main/java/net/glowstone/command/SethomeCommand.java
@@ -1,1 +1,30 @@
+package net.glowstone.command;
+
+import org.bukkit.Location;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.defaults.BukkitCommand;
+import org.bukkit.entity.Player;
+import net.glowstone.GlowServer;
+
+public class SethomeCommand extends BukkitCommand {
+
+	
+	public SethomeCommand() {
+		super("gsesethome");
+		this.description = "Sets the location you're on to your home.";
+		this.usageMessage = "/gsesethome <home name>";
+		this.setPermission("glowstone.command.sethome");
+	}
+	
+	@Override
+    public boolean execute(CommandSender sender, String label, String[] args) {
+        if (sender instanceof Player) {
+        	Player player = (Player) sender;
+        	Location loc = player.getLocation();
+        	GlowServer.getConfig.add(player.getName() + ".home", loc);
+        	player.sendMessage("You're home has been set.");
+        }
+		return false;
+	}
+}
 
