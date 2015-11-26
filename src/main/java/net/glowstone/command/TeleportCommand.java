997f4906561adc2a0940c97b36b573c746a0abec
MrH100000 <h100000@h100000.com>
2015-11-25 22:19:29 -0600

    Fix Checkstyle violations

--- src/main/java/net/glowstone/command/TeleportCommand.java
+++ src/main/java/net/glowstone/command/TeleportCommand.java
@@ -23,16 +23,16 @@ public class TeleportCommand extends BukkitCommand {
     public boolean execute(CommandSender sender, String label, String[] args) {
 
         Player player = (Player) sender;
-        if(args.length == 0){
+        if (args.length == 0) {
             player.sendMessage(ChatColor.RED + usageMessage);
 
-        } else if (args.length == 1){
+        } else if (args.length == 1) {
             Player targetPlayer = player.getServer().getPlayer(args[0]);
             Location targetPlayerLocation = targetPlayer.getLocation();
             player.teleport(targetPlayerLocation);
             sender.sendMessage(ChatColor.GOLD + "Teleporting...");
             return true;
-        } else if(args.length == 2){
+        } else if (args.length == 2) {
             Player targetPlayer = player.getServer().getPlayer(args[0]);
             Player targetPlayer1 = player.getServer().getPlayer(args[1]);
             Location targetPlayer1Location = targetPlayer1.getLocation();
